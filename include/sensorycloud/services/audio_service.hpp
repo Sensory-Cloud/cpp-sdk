da16bb48d4848f68e7e761503c77f008f6550796
Christian Kauten <ckauten@sensoryinc.com>
2021-11-22 10:53:34 -0500

    create audio/video service header file stubs

---
+++ include/sensorycloud/services/audio_service.hpp
@@ -0,0 +1,49 @@
+// The audio service for the Sensory Cloud SDK.
+//
+// Author: Christian Kauten (ckauten@sensoryinc.com)
+//
+// Copyright (c) 2021 Sensory, Inc.
+//
+// Permission is hereby granted, free of charge, to any person obtaining a copy
+// of this software and associated documentation files (the "Software"), to deal
+// in the Software without restriction, including without limitation the rights
+// to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+// copies of the Software, and to permit persons to whom the Software is
+// furnished to do so, subject to the following conditions:
+//
+// The above copyright notice and this permission notice shall be included in
+// all copies or substantial portions of the Software.
+//
+// THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXTERNRESS OR
+// IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+// FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+// AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+// LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+// OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+// SOFTWARE.
+//
+
+#ifndef SENSORY_CLOUD_SERVICES_AUDIO_SERVICE_HPP_
+#define SENSORY_CLOUD_SERVICES_AUDIO_SERVICE_HPP_
+
+#include "sensorycloud/generated/v1/audio/audio.pb.h"
+#include "sensorycloud/generated/v1/audio/audio.grpc.pb.h"
+#include "sensorycloud/config.hpp"
+#include "sensorycloud/services/service.hpp"
+
+/// @brief The Sensory Cloud SDK.
+namespace sensory {
+
+/// @brief Sensory Cloud Services.
+namespace service {
+
+/// @brief A service for audio services.
+class AudioService {
+
+};
+
+}  // namespace service
+
+}  // namespace sensory
+
+#endif  // SENSORY_CLOUD_SERVICES_AUDIO_SERVICE_HPP_
