558e75852a40e3fbb407ea1770e5e2128e3c7af1
Christian Kauten <ckauten@sensoryinc.com>
2021-11-23 03:19:15 -0500

    stub TokenManager

---
+++ include/sensorycloud/token_manager/token_manager.hpp
@@ -0,0 +1,39 @@
+// An OAuth Token manager Sensory Cloud C++ SDK.
+//
+// Author: Christian Kauten (ckauten@sensoryinc.com)
+//
+// Copyright (c) 2021 Sensory, Inc.
+//
+// Permission is hereby granted, free of charge, to any person obtaining a copy
+// of this software and associated documentation files (the "Software"), to deal
+// in the Software without restriction, including without limitation the rights
+// to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+// copies of the Software, and to permit persons to whom the Software is
+// furnished to do so, subject to the following conditions:
+//
+// The above copyright notice and this permission notice shall be included in
+// all copies or substantial portions of the Software.
+//
+// THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXTERNRESS OR
+// IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+// FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+// AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+// LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+// OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+// SOFTWARE.
+//
+
+#ifndef SENSORY_CLOUD_TOKEN_MANAGER_HPP_
+#define SENSORY_CLOUD_TOKEN_MANAGER_HPP_
+
+/// @brief The Sensory Cloud SDK.
+namespace sensory {
+
+/// @brief Modules for generating and storing secure credentials.
+namespace token_manager {
+
+}  // namespace token_manager
+
+}  // namespace sensory
+
+#endif  // SENSORY_CLOUD_TOKEN_MANAGER_HPP_
