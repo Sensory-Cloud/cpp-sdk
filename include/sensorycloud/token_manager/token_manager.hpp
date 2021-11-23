b5448886de83b6708af94cf97bbdb855d8975b48
Christian Kauten <ckauten@sensoryinc.com>
2021-11-23 13:53:25 -0500

    implement TokenManager first pass

--- include/sensorycloud/token_manager/token_manager.hpp
+++ include/sensorycloud/token_manager/token_manager.hpp
@@ -26,11 +26,151 @@
 #ifndef SENSORY_CLOUD_TOKEN_MANAGER_TOKEN_MANAGER_HPP_
 #define SENSORY_CLOUD_TOKEN_MANAGER_TOKEN_MANAGER_HPP_
 
+#include "uuid.hpp"
+#include "secure_random.hpp"
+#include "time.hpp"
+#include "keychain.hpp"
+
 /// @brief The Sensory Cloud SDK.
 namespace sensory {
 
 /// @brief Modules for generating and storing secure credentials.
 namespace token_manager {
+
+/// @brief A wrapper struct for OAuth token credentials.
+struct AccessTokenCredentials {
+    /// The OAuth client id
+    std::string clientID;
+    /// The OAuth client secret
+    std::string secret;
+};
+
+/// @brief A token manager class for generating OAuth tokens.
+/// @tparam SecureCredentialStore a secure CRUD class for storing credentials.
+template<typename SecureCredentialStore>
+class TokenManager {
+ private:
+    /// the key-chain to interact with to store / query key-value pairs
+    SecureCredentialStore& keychain;
+
+ public:
+    /// @brief String keys for accessing secure values in the keychain
+    enum class KeychainTag {
+        /// the ID of the client device (A RFC-4122v4 UUID)
+        ClientID     = "clientID",
+        /// the client secret (a cryptographically secure random number)
+        ClientSecret = "clientSecret",
+        /// the OAuth token from the server
+        AccessToken  = "accessToken",
+        /// the expiration time of the OAuth token
+        Expiration   = "expiration"
+    };
+
+    /// @brief Initialize a new token manager.
+    ///
+    /// @param keychain_ the key-chain to query secure credentials from
+    ///
+    explicit TokenManager(SecureCredentialStore& keychain_) :
+        keychain(keychain_) { }
+
+    /// @brief Generate and store a new set of OAuth credentials.
+    ///
+    /// @returns The generated OAuth credentials
+    /// @throws An error if credentials cannot be securely generated or if the
+    /// credentials cannot be stored in the Keychain.
+    /// @details
+    /// This function will overwrite any other credentials that have been
+    /// generated using this function.
+    ///
+    inline AccessTokenCredentials generateCredentials() const {
+        // Generate a new client ID and secure random secrete string
+        const auto clientID = uuid_v4();
+        const auto secret = secure_random<16>();
+        // Insert the clientID and secret into the persistent credential store
+        keychain.insert(KeychainTag::ClientID, clientID);
+        keychain.insert(KeychainTag::ClientSecret, secret);
+        // Return a new access token with the credentials
+        return AccessTokenCredentials{clientID, secret};
+    }
+
+    /// @brief Determine if any credentials are stored on device.
+    ///
+    /// @returns `true` if any credentials are found, `false` otherwise
+    ///
+    inline bool hasSavedCredentials() const {
+        return keychain.has(KeychainTag::ClientID) && keychain.has(KeychainTag::ClientSecret);
+    }
+
+    /// @brief Return a valid access token for Sensory Cloud gRPC calls.
+    ///
+    /// @throws An error if one occurs while retrieving the saved token, or if
+    /// an error occurs while requesting a new one
+    /// @returns A valid access token
+    /// @details
+    /// This function will immediately return if the cached access token is
+    /// still valid. If a new token needs to be requested, this function will
+    /// block on the current thread until a new token has been fetched from
+    /// the server.
+    ///
+    std::string getAccessToken() const {
+        static constexpr std::chrono::seconds tokenExpirationBuffer = std::chrono::seconds(300);
+
+        // TODO: Prevent multiple access tokens from being requested at the same time
+
+        if (
+            !keychain.has(KeychainTag::AccessToken) ||
+            !keychain.has(KeychainTag::Expiration)
+        )  // no access token has been stored in the credential store
+            return fetchNewAccessToken();
+        // fetch existing access token and expiration date from the secure store
+        const auto accessToken = keychain.get(KeychainTag::AccessToken);
+        const auto expirationDate = keychain.get(KeychainTag::Expiration);
+        // check for expiration of the token
+        const auto now = std::chrono::system_clock::now();
+        const auto expiration = timestamp_to_timepoint(expirationDate);
+        if (now > expiration - tokenExpirationBuffer) {  // token has expired
+            // std::cout << "Cached access token has expired, requesting new token" << std::endl;
+            return fetchNewAccessToken();
+        }
+        // std::cout << "Returning cached access token" << std::endl;
+        return accessToken;
+    }
+
+    /// @brief Delete any credentials stored for requesting access tokens, as
+    /// well as any cached access tokens on device
+    ///
+    /// @returns An error if one occurs during deletion
+    ///
+    inline void deleteCredentials() const {
+        keychain.remove(KeychainTag::AccessToken);
+        keychain.remove(KeychainTag::Expiration);
+        keychain.remove(KeychainTag::ClientID);
+        keychain.remove(KeychainTag::ClientSecret);
+    }
+
+    /// Fetches a new access token from a remote server
+    std::string fetchNewAccessToken() const {
+        if (!keychain.has(KeychainTag::ClientID)) {
+            // TODO: raise exception
+        }
+        if (!keychain.has(KeychainTag::ClientSecret)) {
+            // TODO: raise exception
+        }
+        // Get the ID of the client and the secret from the secure store.
+        const auto clientID = keychain.get(KeychainTag::ClientID);
+        const auto secret = keychain.get(KeychainTag::ClientSecret);
+        // // Synchronously request a new token from the server.
+        // const auto result = service.getToken(clientID, secret);
+        // // Insert the OAuth access token for the client in the secure store
+        // keychain.insert(KeychainTag::AccessToken, result.accessToken);
+        // // Determine when the token will expire and store this time
+        // const auto expiration = std::chrono::system_clock::now() + std::chrono::seconds(result.expiresIn);
+        // const auto expirationDate = timepoint_to_timestamp(expiration);
+        // try keychain.insert(KeychainTag::Expiration, expirationDate);
+        // // Return the newly created OAuth token
+        // return result.accessToken;
+    }
+};
 
 }  // namespace token_manager
 
