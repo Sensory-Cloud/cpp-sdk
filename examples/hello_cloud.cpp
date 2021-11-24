2eef369327f13d635e0476c6e9a7c81e3065eec6
Christian Kauten <ckauten@sensoryinc.com>
2021-11-24 14:43:44 -0500

    update hello cloud to output a token

--- examples/hello_cloud.cpp
+++ examples/hello_cloud.cpp
@@ -24,31 +24,67 @@
 //
 
 #include <iostream>
-// #include <grpc/grpc.h>
-// #include <grpc++/channel.h>
-// #include <grpc++/client_context.h>
-// #include <grpc++/create_channel.h>
-// #include <grpc++/security/credentials.h>
-// #include <sensorycloud/sensorycloud.hpp>
 #include <sensorycloud/config.hpp>
-// #include <sensorycloud/services/management_service.hpp>
+#include <sensorycloud/services/management_service.hpp>
 #include <sensorycloud/services/oauth_service.hpp>
-// #include <sensorycloud/services/audio_service.hpp>
-// #include <sensorycloud/services/video_service.hpp>
+#include <sensorycloud/services/audio_service.hpp>
+#include <sensorycloud/services/video_service.hpp>
+#include <sensorycloud/token_manager/uuid.hpp>
+#include <sensorycloud/token_manager/secure_random.hpp>
+#include <sensorycloud/token_manager/keychain.hpp>
+#include <sensorycloud/token_manager/time.hpp>
+#include <sensorycloud/token_manager/token_manager.hpp>
 
 int main() {
     // Initialize the configuration to the host for given address and port
     std::string host = "io.stage.cloud.sensory.com";
-    uint16_t port = 50051;
+    uint16_t port = 443;
     std::shared_ptr<sensory::Config> config(new sensory::Config);
     config->setCloudHost(host, port);
+    std::cout << config->getCloudHost()->getFullyQualifiedDomainName() << std::endl;
     // Set the Tenant ID for the default tenant
     config->tenantID = "cabb7700-206f-4cc7-8e79-cd7f288aa78d";
+    // a dummy device ID for enrolling in the cloud
+    config->deviceID = "D895F447-91E8-486F-A783-6E3A33E4C7C5";
+
+    sensory::token_manager::Keychain keychain("com.sensory.cloud");
+    //
+    // keychain.remove("clientID");
+    // keychain.remove("clientSecret");
+    //
+    // keychain.insert("clientID", sensory::token_manager::uuid_v4());
+    // std::cout << keychain.get("clientID") << std::endl;
+    //
+    // keychain.insert("clientSecret", sensory::token_manager::secure_random<16>());
+    // std::cout << keychain.get("clientSecret") << std::endl;
 
     // Create an OAuth service from the base service
     auto oauthService = sensory::service::OAuthService(config->getCloudHost()->getChannel());
 
-    auto response = oauthService.getWhoAmI(*config);
+    // oauthService.getWhoAmI(*config);
+
+    std::string userID = "";
+    std::cout << "user ID: ";
+    std::cin >> userID;
+
+    std::string password = "";
+    std::cout << "password: ";
+    std::cin >> password;
+
+    const auto clientID = keychain.get("clientID");
+    const auto clientSecret = keychain.get("clientSecret");
 
     std::cout << "Hello, Sensory Cloud C++ SDK!" << std::endl;
+
+    if (false) {
+        const auto rsp = oauthService.enrollDevice(*config, userID, password, clientID, clientSecret);
+        std::cout << "Your user name is \"" << rsp.name() << "\"" << std::endl;
+        std::cout << "Your device ID is \"" << rsp.deviceid() << "\"" << std::endl;
+    }
+
+    const auto rsp = oauthService.getToken(clientID, clientSecret);
+    std::cout << "Your current token is " << rsp.accesstoken() << std::endl;
+
+    // sensory::token_manager::TokenManager<sensory::token_manager::Keychain> token_manager(oauthService, keychain);
+    // const auto access_token = token_manager.getAccessToken();
 }
