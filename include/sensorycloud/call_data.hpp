14ad997529e8f59ddd0cafae211f1d1fe1683dbc
Christian Kauten <ckauten@sensoryinc.com>
2021-12-03 21:55:52 -0500

    switch grpc++ to grpcpp (grpc++ is deprecated)

--- include/sensorycloud/call_data.hpp
+++ include/sensorycloud/call_data.hpp
@@ -27,7 +27,7 @@
 #define SENSORY_CLOUD_CALL_DATA_HPP_
 
 #include <grpc/grpc.h>
-#include <grpc++/client_context.h>
+#include <grpcpp/client_context.h>
 #include <atomic>
 
 /// @brief The Sensory Cloud SDK.
@@ -79,6 +79,83 @@ struct CallData {
     /// This assignment operator is private to prevent copying of this object.
     ///
     void operator=(const CallData& other) = delete;
+
+    // Mark the Factory type as a friend to allow it to have write access to
+    // the internal types. This allows the parent scope to have mutability, but
+    // all other scopes must access data through the immutable `get` interface.
+    friend Factory;
+
+ public:
+    /// @brief Wait for the asynchronous call to complete.
+    ///
+    /// @details
+    /// This will block the calling thread until the asynchronous call returns
+    /// with a response.
+    ///
+    inline void await() { while (!isDone) continue; }
+
+    /// @brief Return the context that the call was created with.
+    inline const ::grpc::ClientContext& getContext() const { return context; }
+
+    /// @brief Return the status of the call.
+    inline const ::grpc::Status& getStatus() const { return status; }
+
+    /// @brief Return the request that initiated the call.
+    inline const Request& getRequest() const { return request; }
+
+    /// @brief Return the response of the call.
+    inline const Response& getResponse() const { return response; }
+
+    /// @brief Return `true` if the call has resolved, `false` otherwise.
+    inline const bool& getIsDone() const { return isDone; }
+};
+
+/// @brief A type for encapsulating data for asynchronous calls.
+/// @tparam Factory The factory class that will manage the scope of the call.
+/// @tparam Request The type of the request message.
+/// @tparam Response The type of the response message.
+///
+/// @details
+/// The `Factory` is marked as a friend in order to provide mutable access to
+/// private attributes of the structure. This allows instances of `Factory` to
+/// mutate to the structure while all external scopes are limited to the
+/// immutable interface exposed by the public accessor functions. Instances of
+/// `BidiCallData` are mutable within the scope of `Factory`, but immutable
+/// outside of the scope of `Factory`.
+///
+template<typename Factory, typename Request, typename Response>
+struct BidiCallData {
+ private:
+    /// The gPRC context that the call is initiated with.
+    ::grpc::ClientContext context;
+    /// The status of the RPC after the response is processed.
+    ::grpc::Status status;
+    /// The request to execute in the unary call.
+    Request request;
+    /// The response to process after the RPC completes.
+    Response response;
+    /// A flag determining whether the asynchronous has terminated.
+    std::atomic<bool> isDone;
+
+    /// @brief Initialize a new call.
+    BidiCallData() : isDone(false) { }
+
+    /// @brief Create a copy of this object.
+    ///
+    /// @param other the other instance to copy data from
+    ///
+    /// @details
+    /// This copy constructor is private to prevent the copying of this object
+    BidiCallData(const BidiCallData& other) = delete;
+
+    /// @brief Assign to this object using the `=` operator.
+    ///
+    /// @param other The other instance to copy data from.
+    ///
+    /// @details
+    /// This assignment operator is private to prevent copying of this object.
+    ///
+    void operator=(const BidiCallData& other) = delete;
 
     // Mark the Factory type as a friend to allow it to have write access to
     // the internal types. This allows the parent scope to have mutability, but
--- include/sensorycloud/config.hpp
+++ include/sensorycloud/config.hpp
@@ -27,10 +27,10 @@
 #define SENSORY_CLOUD_CONFIG_HPP_
 
 #include <grpc/grpc.h>
-#include <grpc++/channel.h>
-#include <grpc++/client_context.h>
-#include <grpc++/create_channel.h>
-#include <grpc++/security/credentials.h>
+#include <grpcpp/channel.h>
+#include <grpcpp/client_context.h>
+#include <grpcpp/create_channel.h>
+#include <grpcpp/security/credentials.h>
 #include <memory>
 #include <algorithm>
 #include <cstdint>
