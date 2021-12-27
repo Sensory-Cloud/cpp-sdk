a8b855fe514755973aaf6b44606c775d20cc45a3
Christian Kauten <ckauten@sensoryinc.com>
2021-12-27 10:24:05 -0700

    update to expect local gRPC in different directory

--- cmake/common.cmake
+++ cmake/common.cmake
@@ -49,9 +49,8 @@ if(GRPC_AS_SUBMODULE)
     # cmake's ExternalProject_Add (see cmake_externalproject/CMakeLists.txt).
 
     # Include the gRPC's cmake build (normally grpc source code would live
-    # in a git submodule called "third_party/grpc", but this example lives in
-    # the same repository as gRPC sources, so we just look a few directories up)
-    add_subdirectory(../../.. ${CMAKE_CURRENT_BINARY_DIR}/grpc EXCLUDE_FROM_ALL)
+    # in a git submodule called "third_party/grpc")
+    add_subdirectory(third_party/grpc ${CMAKE_CURRENT_BINARY_DIR}/grpc EXCLUDE_FROM_ALL)
     message(STATUS "Using gRPC via add_subdirectory.")
 
     # After using add_subdirectory, we can now use the grpc targets directly from
